22cfd91d8f3f866a273081e4021f53349ac8a2c3
Akshay <akshaypanampatta8891@gmail.com>
2023-08-17 10:26:07 +0530

    added profile, Header section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Navbar from './Components/Navbar/Navbar'
+import Skills from './Components/Skills/Skills'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
    <Navbar/>
    <Header/>
+   <Skills/>
     </>
   )
 }
