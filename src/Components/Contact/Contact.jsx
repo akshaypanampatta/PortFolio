8253d3f369146d08b327cd29d36f0736612012b0
Akshay <akshaypanampatta8891@gmail.com>
2023-08-18 16:22:28 +0530

    added functionality for contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./Contact.css";
 import { FiSend } from "react-icons/fi";
 
-
 const Contact = () => {
   return (
     <>
@@ -17,14 +16,18 @@ const Contact = () => {
           <div className="container px-5 py-20 mx-auto">
             <div className="flex flex-col text-center w-full mb-12">
               <h1 className="contact sm:text-4xl text-2xl font-medium title-font mb-4 text-white">
-                Need a Stunning Website <span className="text-orange-500">Design?</span>
+                Need a Stunning Website{" "}
+                <span className="text-orange-500">Design?</span>
               </h1>
               <p className="contact-sub lg:w-2/3 mx-auto leading-relaxed ">
                 Request a Beautifully Crafted Website for Your Business
               </p>
             </div>
             <div className="lg:w-1/2 md:w-2/3 mx-auto">
-              <div className="flex flex-wrap -m-2">
+              <form
+                action="mailto:akshaypanampatta8891@gmail.com"
+                className="flex flex-wrap -m-2"
+              >
                 <div className="p-2 w-1/2">
                   <div className="relative">
                     <label for="name" className="leading-7 text-sm text-white">
@@ -34,7 +37,8 @@ const Contact = () => {
                       type="text"
                       id="name"
                       name="name"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                      placeholder="e.g. John Doe"
+                      className="w-full  bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     />
                   </div>
                 </div>
@@ -45,9 +49,10 @@ const Contact = () => {
                     </label>
                     <input
                       type="email"
+                      placeholder="e.g. example@email.com"
                       id="email"
                       name="email"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                      className="w-full  bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                     />
                   </div>
                 </div>
@@ -62,17 +67,17 @@ const Contact = () => {
                     <textarea
                       id="message"
                       name="message"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                      placeholder="Write your message"
+                      className="w-full  bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                     ></textarea>
                   </div>
                 </div>
                 <div className="p-2 w-full">
-                  <button className="send-button mx-auto flex items-center justify-center">
+                  <button type="submit" className="send-button mx-auto flex items-center justify-center">
                     Send <FiSend className="ml-2" />
                   </button>
                 </div>
-               
-              </div>
+              </form>
             </div>
           </div>
         </section>
