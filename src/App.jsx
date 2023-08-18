889fe08aec49ad3089bc40eefdef28a9a3a58721
Akshay <akshaypanampatta8891@gmail.com>
2023-08-18 16:35:50 +0530

    added homr page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import './App.css'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
-import Header from './Components/Header/Header'
-import Navbar from './Components/Navbar/Navbar'
-import Skills from './Components/Skills/Skills'
-import Works from './Components/Works/Works'
+import Homepage from './Pages/Homepage'
+
 
 function App() {
   
 
   return (
     <>
-   <Navbar/>
-   <Header/>
-   <Skills/>
-   <Works/>
-   <Contact/>
-   <Footer/>
+   <Homepage/>
     </>
   )
 }
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from "../Components/Navbar/Navbar"
+import Header from '../Components/Header/Header'
+import Skills from '../Components/Skills/Skills'
+import Works from '../Components/Works/Works'
+import Footer from '../Components/Footer/Footer'
+
+const Homepage = () => {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+    <Skills/>
+    <Works/>
+    <Footer/>
+
+    </>
+  )
+}
+
+export default Homepage
